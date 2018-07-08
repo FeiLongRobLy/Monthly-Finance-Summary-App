389550aea276a5021d290ac55d5aa4981b4cf152
FeiLong001 <azn69_vigalanti@hotmail.com>
2018-07-08 16:59:04 +1000

    Second commit, updated some JMenu stuff and created some action listeners.

--- src/GUI.java
+++ src/GUI.java
@@ -1,14 +1,21 @@
 import javax.swing.*;
 import java.awt.FlowLayout;
 
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 public class GUI extends JFrame
 {
-    public static void createFrame() {
+    public static void createFrame()
+    {
         //defining the JMenu items
         JPanel panel = new JPanel();
         JFrame frame = new JFrame("Monthly Finance Summary");
         JMenuBar menuBar = new JMenuBar();
-        JMenu menu = new JMenu("File");
+        JMenu fileMenu = new JMenu("File");
+        JMenu aboutMenu = new JMenu("Help");
+        JMenuItem exitMenuItem = new JMenuItem("Exit");
+        JMenuItem aboutMenuItem = new JMenuItem("About");
 
         panel.setLayout(new FlowLayout());
 
@@ -16,10 +23,35 @@ public class GUI extends JFrame
         frame.add(panel);
 
         //menu bar stuff
-        menuBar.add(menu);
+        menuBar.add(fileMenu);
+        menuBar.add(aboutMenu);
 
+        fileMenu.add(exitMenuItem);
+        aboutMenu.add(aboutMenuItem);
+
+        //action listeners
+        exitMenuItem.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent ev)
+            {
+                System.exit(0);
+            }
+        });
+
+        //action listeners
+        aboutMenuItem.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent ev)
+            {
+                 JOptionPane.showMessageDialog(null, "Created By Robert Ly © 2018");
+            }
+        });
+
+        //frame stuff
         frame.add(menuBar);
+        frame.setJMenuBar(menuBar);
         frame.setSize(800,800);
         frame.setVisible(true);
+
     }
 }
